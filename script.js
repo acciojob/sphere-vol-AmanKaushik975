4a827d29efbca1493056b3a4e8bcd29086e0515e
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-04-30 10:06:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function calculateVolume() {
+  // Get the radius input value
+  var radius = document.getElementById("radius").value;
+if(radius<0)
+{
+	document.getElementbyId("result").innerHTML="The volume of the sphere is" +NAN;
+	return
+}
+	
+  // Calculate the volume of the sphere
+  var volume = (4/3) * Math.PI * Math.pow(radius, 3);
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+  // Display the resul
+  document.getElementById("result").innerHTML = "The volume of the sphere is: " + volume.toFixed(4);
+}
+
+window.onload = document.getElementById('MyForm').onclick = calculateVolume();
